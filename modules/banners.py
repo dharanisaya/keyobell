99294b8d5b64717f67f5f6df3d8bf059c929093c
dharanisaya <dharanipriyasivakumar@gmail.com>
2024-10-04 15:02:27 +0530

    Add files via upload

--- modules/banners.py
+++ modules/banners.py
@@ -10,32 +10,13 @@ def print_banner(console) -> None:
     height = 8
     banner = """\
  
-                                                                                                                                
-                                                                         bbbbbbbb                                               
-kkkkkkkk                                                                 b::::::b                               lllllll lllllll 
-k::::::k                                                                 b::::::b                               l:::::l l:::::l 
-k::::::k                                                                 b::::::b                               l:::::l l:::::l 
-k::::::k                                                                  b:::::b                               l:::::l l:::::l 
- k:::::k    kkkkkkk eeeeeeeeeeee  yyyyyyy           yyyyyyy ooooooooooo   b:::::bbbbbbbbb        eeeeeeeeeeee    l::::l  l::::l 
- k:::::k   k:::::kee::::::::::::ee y:::::y         y:::::yoo:::::::::::oo b::::::::::::::bb    ee::::::::::::ee  l::::l  l::::l 
- k:::::k  k:::::ke::::::eeeee:::::eey:::::y       y:::::yo:::::::::::::::ob::::::::::::::::b  e::::::eeeee:::::eel::::l  l::::l 
- k:::::k k:::::ke::::::e     e:::::e y:::::y     y:::::y o:::::ooooo:::::ob:::::bbbbb:::::::be::::::e     e:::::el::::l  l::::l 
- k::::::k:::::k e:::::::eeeee::::::e  y:::::y   y:::::y  o::::o     o::::ob:::::b    b::::::be:::::::eeeee::::::el::::l  l::::l 
- k:::::::::::k  e:::::::::::::::::e    y:::::y y:::::y   o::::o     o::::ob:::::b     b:::::be:::::::::::::::::e l::::l  l::::l 
- k:::::::::::k  e::::::eeeeeeeeeee      y:::::y:::::y    o::::o     o::::ob:::::b     b:::::be::::::eeeeeeeeeee  l::::l  l::::l 
- k::::::k:::::k e:::::::e                y:::::::::y     o::::o     o::::ob:::::b     b:::::be:::::::e           l::::l  l::::l 
-k::::::k k:::::ke::::::::e                y:::::::y      o:::::ooooo:::::ob:::::bbbbbb::::::be::::::::e         l::::::ll::::::l
-k::::::k  k:::::ke::::::::eeeeeeee         y:::::y       o:::::::::::::::ob::::::::::::::::b  e::::::::eeeeeeee l::::::ll::::::l
-k::::::k   k:::::kee:::::::::::::e        y:::::y         oo:::::::::::oo b:::::::::::::::b    ee:::::::::::::e l::::::ll::::::l
-kkkkkkkk    kkkkkkk eeeeeeeeeeeeee       y:::::y            ooooooooooo   bbbbbbbbbbbbbbbb       eeeeeeeeeeeeee llllllllllllllll
-                                        y:::::y                                                                                 
-                                       y:::::y                                                                                  
-                                      y:::::y                                                                                   
-                                     y:::::y                                                                                    
-                                    yyyyyyy                                                                                     
-                                                                                                                                
-                                                                                                                                
-
+ _                        _            _  _ 
+| | __ ___  _   _   ___  | |__    ___ | || |
+| |/ // _ \| | | | / _ \ | '_ \  / _ \| || |
+|   <|  __/| |_| || (_) || |_) ||  __/| || |
+|_|\_\\___| \__, | \___/ |_.__/  \___||_||_|
+            |___/                           
+                                                                                    
 
 """
 
@@ -48,6 +29,6 @@ kkkkkkkk    kkkkkkk eeeeeeeeeeeeee       y:::::y            ooooooooooo   bbbbbb
         ),
         width=width,
         height=height,
-        subtitle="by dharanisaya (https://auto.pwnspot.com)",
+        subtitle="by dharanisaya (https://github.com/dharanisaya/keyobell.git)",
     )
     console.print(panel)
