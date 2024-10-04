85fb9d71616625d0e59c79243682fa444a33bdc7
dharanisaya <dharanipriyasivakumar@gmail.com>
2024-10-04 15:00:19 +0530

    Add files via upload

--- modules/banners.py
+++ modules/banners.py
@@ -9,28 +9,36 @@ def print_banner(console) -> None:
     width = get_terminal_width()
     height = 8
     banner = """\
-   _               _  _           _                 _       _             
-  | |__    ___    | || |   ___   | |__     ___     | |     | |      o O O 
-  | / /   / -_)    \_, |  / _ \  | '_ \   / -_)    | |     | |     o      
-  |_\_\   \___|   _|__/   \___/  |_.__/   \___|   _|_|_   _|_|_   TS__[O] 
-_|"""""|_|"""""|_| """"|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| {======| 
-"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'
+ 
+                                                                                                                                
+                                                                         bbbbbbbb                                               
+kkkkkkkk                                                                 b::::::b                               lllllll lllllll 
+k::::::k                                                                 b::::::b                               l:::::l l:::::l 
+k::::::k                                                                 b::::::b                               l:::::l l:::::l 
+k::::::k                                                                  b:::::b                               l:::::l l:::::l 
+ k:::::k    kkkkkkk eeeeeeeeeeee  yyyyyyy           yyyyyyy ooooooooooo   b:::::bbbbbbbbb        eeeeeeeeeeee    l::::l  l::::l 
+ k:::::k   k:::::kee::::::::::::ee y:::::y         y:::::yoo:::::::::::oo b::::::::::::::bb    ee::::::::::::ee  l::::l  l::::l 
+ k:::::k  k:::::ke::::::eeeee:::::eey:::::y       y:::::yo:::::::::::::::ob::::::::::::::::b  e::::::eeeee:::::eel::::l  l::::l 
+ k:::::k k:::::ke::::::e     e:::::e y:::::y     y:::::y o:::::ooooo:::::ob:::::bbbbb:::::::be::::::e     e:::::el::::l  l::::l 
+ k::::::k:::::k e:::::::eeeee::::::e  y:::::y   y:::::y  o::::o     o::::ob:::::b    b::::::be:::::::eeeee::::::el::::l  l::::l 
+ k:::::::::::k  e:::::::::::::::::e    y:::::y y:::::y   o::::o     o::::ob:::::b     b:::::be:::::::::::::::::e l::::l  l::::l 
+ k:::::::::::k  e::::::eeeeeeeeeee      y:::::y:::::y    o::::o     o::::ob:::::b     b:::::be::::::eeeeeeeeeee  l::::l  l::::l 
+ k::::::k:::::k e:::::::e                y:::::::::y     o::::o     o::::ob:::::b     b:::::be:::::::e           l::::l  l::::l 
+k::::::k k:::::ke::::::::e                y:::::::y      o:::::ooooo:::::ob:::::bbbbbb::::::be::::::::e         l::::::ll::::::l
+k::::::k  k:::::ke::::::::eeeeeeee         y:::::y       o:::::::::::::::ob::::::::::::::::b  e::::::::eeeeeeee l::::::ll::::::l
+k::::::k   k:::::kee:::::::::::::e        y:::::y         oo:::::::::::oo b:::::::::::::::b    ee:::::::::::::e l::::::ll::::::l
+kkkkkkkk    kkkkkkk eeeeeeeeeeeeee       y:::::y            ooooooooooo   bbbbbbbbbbbbbbbb       eeeeeeeeeeeeee llllllllllllllll
+                                        y:::::y                                                                                 
+                                       y:::::y                                                                                  
+                                      y:::::y                                                                                   
+                                     y:::::y                                                                                    
+                                    yyyyyyy                                                                                     
+                                                                                                                                
+                                                                                                                                
 
 
 """
 
-    banner_small = """\
-
-.           .     ..
-;_/ _   . _ |_  _ ||
-| \(/,\_|(_)[_)(/,||
-      ._|           
-
-"""
-
-    if width < 90:
-        banner = banner_small
-        height = 5
 
     panel = Panel(
         Align(
